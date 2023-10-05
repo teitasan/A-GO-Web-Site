9b08fc47cb44098b1b3d580100165bf0af39b325
teitasan <teitasan@gmail.com>
2023-10-06 00:02:25 +0900

    footerにSNSアイコン追加

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -3,6 +3,8 @@
     <div class="footer_logo">
         <a href="{{ route('index') }}" >A-GO</a>
     </div>
+    <a href="https://twitter.com/AGO31094445?ref_src=twsrc%5Etfw" target="_blank" class ="twitter_logo"><img src="{{ asset('/image/logo-white.png')}}"></a>
+    <a href="https://youtube.com/@a-go9368?si=Jnq8gqvc2EbFlV0Q" target="_blank" class ="youtube_logo"><img src="{{ asset('/image/yt_icon_mono_light.png')}}"></a>
     <nav>
         <ul class="footer_menu">
             <li class="footer_item"><a href="{{ route('index') }}">TOP</a></li>
